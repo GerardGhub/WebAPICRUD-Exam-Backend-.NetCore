4d62a0871ac5123812cbd5182e1424aafb42e1be
GerardGhub <singian28@gmail.com>
2022-11-30 22:39:49 +0800

    Add DbContext

---
+++ Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPICRUD.Models;
+
+namespace WebAPICRUD.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
