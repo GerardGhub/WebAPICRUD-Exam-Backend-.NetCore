297fd279a37fa086d4c896c35b0adcb6ba4142cf
GerardGhub <singian28@gmail.com>
2022-11-30 22:59:31 +0800

    Add New ViewModel for Insert and modify contact controller add post method and update the request to async

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPICRUD.Data;
+using WebAPICRUD.Models;
 
 namespace WebAPICRUD.Controllers
 {
@@ -14,9 +16,27 @@ namespace WebAPICRUD.Controllers
             this.dbContext = dbContext;
         }
         [HttpGet]
-        public IActionResult GetContact()
+        public async Task<IActionResult> GetContact()
         {
-           return Ok(dbContext.Contacts.ToList());
+           return Ok(await dbContext.Contacts.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContacRequest addContacRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContacRequest.Address,
+                Email = addContacRequest.Email,
+                FullName = addContacRequest.FullName,
+                Phone = addContacRequest.Phone
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+            return Ok(contact);
+        }
+
     }
 }
---
+++ Models/AddContacRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPICRUD.Models
+{
+    public class AddContacRequest
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
