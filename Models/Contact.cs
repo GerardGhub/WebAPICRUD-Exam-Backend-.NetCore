a54e251cc31c35ee964431d27bc80897fcf5329b
GerardGhub <singian28@gmail.com>
2022-11-30 22:35:03 +0800

    Add package Microsoft.EntityFrameworkCore.InMemory and add domain model for contact

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public long Phone {  get; set; }
+        public long Phone { get; set; }
         public string Address { get; set; }
 
     }
