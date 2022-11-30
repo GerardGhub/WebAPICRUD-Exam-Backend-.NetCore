c688c32786d54e36a9f84b7b213d1021b0e479ce
GerardGhub <singian28@gmail.com>
2022-11-30 22:50:38 +0800

    Add new Contact Controller

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPICRUD.Data;
+
+namespace WebAPICRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetContact()
+        {
+           return Ok(dbContext.Contacts.ToList());
+        }
+    }
+}
